5daaa7b42cc407a7506d84365fd35ff09cdf3f40
brabbitdousha <1216874964@qq.com>
2022-03-09 17:31:52 +0800

    update to 7.2 antialiasing

--- color.h
+++ color.h
@@ -8,12 +8,22 @@ struct int3
 {
 	int x,y,z;
 };
-int3 write_color(color pixel_color) {
+int3 write_color(color pixel_color,int samples_per_pixel) {
+    auto r=pixel_color.x();
+    auto g=pixel_color.y();
+    auto b=pixel_color.z();
+
+    // Divide the color by the number of samples.
+    auto scale=1.0/samples_per_pixel;
+    r*=scale;
+    g*=scale;
+    b*=scale;
+    
     // Write the translated [0,255] value of each color component.
     int3 temp;
-    temp.x=static_cast<int>(255.999 * pixel_color.x());
-    temp.y=static_cast<int>(255.999 * pixel_color.y());
-    temp.z=static_cast<int>(255.999 * pixel_color.z());
+    temp.x=static_cast<int>(255.999 * clamp(r,0.0,0.999));
+    temp.y=static_cast<int>(255.999 * clamp(g,0.0,0.999));
+    temp.z=static_cast<int>(255.999 * clamp(b,0.0,0.999));
     return temp;
 }
 
