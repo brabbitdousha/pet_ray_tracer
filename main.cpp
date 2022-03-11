1abf2040a6af7243b923ed30e65e9efdf574fca8
brabbitdousha <1216874964@qq.com>
2022-03-11 18:10:36 +0800

    update to 9.6

--- main.cpp
+++ main.cpp
@@ -40,8 +40,8 @@ int main() {
     hittable_list world;
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8),0.3);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2),1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
@@ -52,7 +52,7 @@ int main() {
     camera cam;
 
     // Render
-    FILE* fp = fopen("9.5.ppm", "wb");
+    FILE* fp = fopen("9.6.ppm", "wb");
     fprintf(fp, "P3\n%d %d\n255\n",image_width,image_height);
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
