867c9aa06f88fc4a5df823aa9d8d86747d93aac1
brabbitdousha <1216874964@qq.com>
2022-03-11 19:16:08 +0800

    update to 11.1

--- main.cpp
+++ main.cpp
@@ -37,22 +37,20 @@ int main() {
     const int max_depth=50;
 
     //World
+    auto R=cos(pi/4);
     hittable_list world;
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8),0.3);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2),1.0);
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    auto material_left   = make_shared<lambertian>(color(0,0,1));
+    auto material_right  = make_shared<lambertian>(color(1,0,0));
+
+    world.add(make_shared<sphere>(point3(-R, 0.0, -1.0),   R, material_left));
+    world.add(make_shared<sphere>(point3( R, 0.0, -1.0),   R, material_right));
 
     //Camera
-    camera cam;
+    camera cam(90.0,aspect_ratio);
 
     // Render
-    FILE* fp = fopen("9.6.ppm", "wb");
+    FILE* fp = fopen("11.1.ppm", "wb");
     fprintf(fp, "P3\n%d %d\n255\n",image_width,image_height);
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
