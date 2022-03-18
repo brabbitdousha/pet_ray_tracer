75cdd3edd74fa81325a91bbdc8f611d7d84cfe02
brabbitdousha <1216874964@qq.com>
2022-03-18 17:16:03 +0800

    2.Motion Blur

--- camera.h
+++ camera.h
@@ -13,7 +13,9 @@ class camera
         double vfov,
         double aspect_ratio,
         double aperture,
-        double focus_dist
+        double focus_dist,
+        double _time0=0,
+        double _time1=0
     )
     {
         auto theta=degrees_to_radians(vfov);
@@ -32,13 +34,18 @@ class camera
         lower_left_corner=origin-horizontal/2-vertical/2-focus_dist*w;
 
         lens_radius=aperture/2;
+        time0=_time0;
+        time1=_time1;
     }
 
     ray get_ray(double s,double t) const 
     {    vec3 rd=lens_radius*random_in_unit_disk();
          vec3 offset=u*rd.x()+v*rd.y();
 
-        return ray(origin+offset,lower_left_corner+s*horizontal+t*vertical-origin-offset);
+        return ray(origin+offset,
+        lower_left_corner+s*horizontal+t*vertical-origin-offset,
+        random_double(time0,time1)
+        );
     }
     private:
         point3 origin;
@@ -47,5 +54,6 @@ class camera
         vec3 vertical;
         vec3 u,v,w;
         double lens_radius;
+        double time0,time1;
 };
 #endif
