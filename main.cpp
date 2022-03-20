2564664fe722c7937ae8a90c3cbc8c209f8809e7
brabbitdousha <1216874964@qq.com>
2022-03-20 19:57:08 +0800

    update to4.4

--- main.cpp
+++ main.cpp
@@ -78,6 +78,17 @@ hittable_list random_scene()
 
     return world;
 }
+
+hittable_list two_spheres()
+{
+    hittable_list objects;
+    auto checker=make_shared<checker_texture>(color(0.2,0.3,0.1),color(0.9,0.9,0.9));
+
+    objects.add(make_shared<sphere>(point3(0,-10,0),10,make_shared<lambertian>(checker)));
+    objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    return objects;
+}
 int main() {
 
     // Image
@@ -88,16 +99,37 @@ int main() {
     const int max_depth=50;
 
     //World
-   auto world=random_scene();
+   hittable_list world;
 
-    //Camera
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
+   point3 lookfrom;
+   point3 lookat;
+   auto vfov = 40.0;
+   auto aperture = 0.0;
+
+   switch (0) {
+    case 1:
+        world = random_scene();
+        lookfrom = point3(13,2,3);
+        lookat = point3(0,0,0);
+        vfov = 20.0;
+        aperture = 0.1;
+        break;
+
+    default:
+    case 2:
+        world = two_spheres();
+        lookfrom = point3(13,2,3);
+        lookat = point3(0,0,0);
+        vfov = 20.0;
+        break;
+    }
+
+// Camera
+
     vec3 vup(0,1,0);
     auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus,0.0,1.0);
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     // Render
     FILE* fp = fopen("Final scene.ppm", "wb");
