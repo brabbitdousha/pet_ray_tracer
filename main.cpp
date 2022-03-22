3cb9e095141b2d65cb72c6b3b592dd2aa4f57eb1
butters dousha <1216874964@qq.com>
2022-03-22 17:10:37 +0800

    update to 5.7

--- main.cpp
+++ main.cpp
@@ -89,6 +89,16 @@ hittable_list two_spheres()
 
     return objects;
 }
+
+hittable_list two_perlin_spheres()
+{
+    hittable_list objects;
+    auto pertext=make_shared<noise_texture>(4);
+    objects.add(make_shared<sphere>(point3(0,-1000,0),1000,make_shared<lambertian>(pertext)));
+    objects.add(make_shared<sphere>(point3(0,2,0),2,make_shared<lambertian>(pertext)));
+
+    return objects;
+}
 int main() {
 
     // Image
@@ -115,12 +125,19 @@ int main() {
         aperture = 0.1;
         break;
 
-    default:
+    
     case 2:
         world = two_spheres();
         lookfrom = point3(13,2,3);
         lookat = point3(0,0,0);
         vfov = 20.0;
+        break;
+    default:
+    case 3:
+        world=two_perlin_spheres();
+        lookfrom=point3(13,2,3);
+        lookat=point3(0,0,0);
+        vfov=20.0;
         break;
     }
 
